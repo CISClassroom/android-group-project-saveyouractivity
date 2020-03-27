a14c9dc459b32a60b9354094c0038e328b9aae8e
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-03-28 03:49:00 +0700

    check_studentid

--- app/src/main/java/th/ac/kku/cis/saveyouractivity/Menu.kt
+++ app/src/main/java/th/ac/kku/cis/saveyouractivity/Menu.kt
@@ -1,9 +1,12 @@
 package th.ac.kku.cis.saveyouractivity
 
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
 import android.util.Log
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.*
 
@@ -23,6 +26,38 @@ class Menu : AppCompatActivity() {
         mDB =FirebaseDatabase.getInstance().reference.child("Auth")//.child(uid!!)
         add_st()
     }
+    fun check_studentid(){
+        mDB.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.child(uid).child("role").value=="student") {
+                    // it exists!
+                    if(snapshot.child(uid).hasChild("code")){
+
+                    }
+                    else{
+                        add_studentid()
+                    }
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {}
+        })
+    }
+    fun add_studentid(){
+        var taskEditText: EditText =  EditText(this)
+        val dialog: AlertDialog = AlertDialog.Builder(this)
+            .setTitle("Input Student ID")
+            .setMessage("Ex 60XXXXXXX-X")
+            .setView(taskEditText)
+            .setPositiveButton("Save",
+                DialogInterface.OnClickListener { dialog, which ->
+                    val task: String = java.lang.String.valueOf(taskEditText.getText())
+                    mDB.child(uid).child("code").setValue(task)
+                })
+            // .setNegativeButton("Cancel", null)
+            .create()
+        dialog.show()
+    }
     fun add_st(){
         mDB.addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
@@ -35,9 +70,11 @@ class Menu : AppCompatActivity() {
                     }
                     else{
                         mDB.child(uid).child("role").setValue("student")
+
                     }
                 }
                 mDB.child(uid).child("name").setValue(USER.getname())
+                check_studentid()
             }
 
             override fun onCancelled(databaseError: DatabaseError) {}
