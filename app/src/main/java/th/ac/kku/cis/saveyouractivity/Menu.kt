6c31dc4c91c6c63557cebd619b60fdf5fb5c32fc
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-03-28 01:54:06 +0700

    Add role
    Co-Authored-By: jariyapun <jariyapun@users.noreply.github.com>
    Co-Authored-By: นายนฤเบศร์  พระโรจน์ <marksolution@users.noreply.github.com>

--- app/src/main/java/th/ac/kku/cis/saveyouractivity/Menu.kt
+++ app/src/main/java/th/ac/kku/cis/saveyouractivity/Menu.kt
@@ -1,12 +1,45 @@
 package th.ac.kku.cis.saveyouractivity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.*
+
 
 class Menu : AppCompatActivity() {
-
+    lateinit var auth: FirebaseAuth
+    lateinit var mDB: DatabaseReference
+    var uid:String=""
+    lateinit var user:FirebaseUser
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+        auth = FirebaseAuth.getInstance()
+        user=auth.currentUser!!
+        uid = user!!.uid
+        mDB =FirebaseDatabase.getInstance().reference.child("Auth")//.child(uid!!)
+        add_st()
+    }
+    fun add_st(){
+        mDB.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.hasChild(uid)) {
+
+                    // it exists!
+                } else {
+                    // does not exist
+                    if("kku.ac.th" in user.email.toString()){
+                        mDB.child(uid).child("role").setValue("teacher")
+                    }
+                    else{
+                        mDB.child(uid).child("role").setValue("student")
+                    }
+                }
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {}
+        })
+
     }
 }
