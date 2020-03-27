14c565c2af0aae4e0e601c6fc3ceeea249ccec8c
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-03-28 02:32:10 +0700

    Add ShowActivity
    Co-Authored-By: jariyapun <jariyapun@users.noreply.github.com>
    Co-Authored-By: นายนฤเบศร์  พระโรจน์ <marksolution@users.noreply.github.com>

---
+++ app/src/main/java/th/ac/kku/cis/saveyouractivity/ShowActivity.kt
@@ -0,0 +1,12 @@
+package th.ac.kku.cis.saveyouractivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show)
+    }
+}
