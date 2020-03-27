c6e8ae404f64a725d233d49cae11ff697c7a3fd6
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-03-28 00:39:38 +0700

    Add Google auth
    Co-Authored-By: jariyapun <jariyapun@users.noreply.github.com>
    Co-Authored-By: นายนฤเบศร์  พระโรจน์ <marksolution@users.noreply.github.com>

--- app/src/main/java/th/ac/kku/cis/saveyouractivity/MainActivity.kt
+++ app/src/main/java/th/ac/kku/cis/saveyouractivity/MainActivity.kt
@@ -1,14 +1,92 @@
 package th.ac.kku.cis.saveyouractivity
 
+import android.Manifest
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var auth: FirebaseAuth
+    lateinit var googleClient: GoogleSignInClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         if (supportActionBar != null)
             supportActionBar?.hide()
+
+        auth = FirebaseAuth.getInstance()
+        var gso = GoogleSignInOptions.Builder(
+            GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleClient = GoogleSignIn.getClient(this,gso)
+        auth = FirebaseAuth.getInstance()
+        sign_in.setOnClickListener { singIn() }
+    }
+    private fun singOut() {
+        auth.signOut()
+        googleClient.signOut().addOnCompleteListener(this) {
+            updateUI(null)
+        }
+    }
+    private fun updateUI(user: FirebaseUser?) {
+        if(user==null){
+            //  show.text = "No User"
+        }
+        else{
+            //show.text = user.email.toString()
+            checklogin()
+        }
+    }
+    private fun checklogin(){
+        if(auth.currentUser!=null){
+
+        }
+    }
+
+    private fun singIn() {
+        singOut()
+        var signInInent = googleClient.signInIntent
+        startActivityForResult(signInInent,101)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode==101){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try{
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuth(account!!)
+                //FirebaseAuth(account)
+            }catch (e: ApiException){
+                Log.w("login",e)
+                updateUI(null)
+            }
+        }
+    }
+
+    private fun firebaseAuth(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken,null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this){task->
+                if(task.isSuccessful){
+                    val user = auth.currentUser
+                    updateUI(user)
+                }
+                else{
+                    updateUI(null)
+                }
+            }
     }
 }
