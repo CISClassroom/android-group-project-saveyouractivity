7ea3a2e903b5f438300168eb337627bf25969525
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-03-29 01:07:32 +0700

    Update item
    Co-Authored-By: jariyapun <jariyapun@users.noreply.github.com>
    Co-Authored-By: นายนฤเบศร์  พระโรจน์ <marksolution@users.noreply.github.com>

---
+++ app/src/main/java/th/ac/kku/cis/saveyouractivity/ActivityAdapter.kt
@@ -0,0 +1,62 @@
+package th.ac.kku.cis.saveyouractivity
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import android.widget.BaseAdapter
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+
+class ActivityAdapter(context: Context, ItemList: MutableList<ActivityItem>) : BaseAdapter() {
+
+    private val mInflater: LayoutInflater = LayoutInflater.from(context)
+    var contextthis: Context = context
+    private var itemList = ItemList
+    lateinit var mDatabase: DatabaseReference
+    lateinit var auth: FirebaseAuth
+    var uid: String? = null
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        // create object from view
+        val objectId: String? = itemList.get(position).objID as String?
+        var itemName: String? = itemList.get(position).Name as String?
+        val itemDate: String? = itemList.get(position).ADate as String?
+        val view: View
+        val vh: ListRowHolder
+
+
+
+
+        // get list view
+        if (convertView == null) {
+            view = mInflater.inflate(R.layout.content_item_activity, parent, false)
+            vh = ListRowHolder(view)
+            view.tag = vh
+        } else {
+            view = convertView
+            vh = view.tag as ListRowHolder
+        }
+        vh.Name.text = itemName
+        vh.ADate.text = itemDate
+        return view
+    }
+
+    override fun getItem(index: Int): Any {
+        return itemList.get(index)
+    }
+
+    override fun getItemId(index: Int): Long {
+        return index.toLong()
+    }
+
+    override fun getCount(): Int {
+        return itemList.size
+    }
+
+    private class ListRowHolder(row: View?) {
+        val Name: TextView = row!!.findViewById<TextView>(R.id.AName) as TextView
+        val ADate:TextView = row!!.findViewById<TextView>(R.id.ADate) as TextView
+    }
+}
