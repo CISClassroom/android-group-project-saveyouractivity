0a88639766ffb40e1a0fa3beae8d4e7c58979a45
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-03-28 23:41:47 +0700

    Update Code and Add QrCode
    Co-Authored-By: jariyapun <jariyapun@users.noreply.github.com>

--- app/src/main/java/th/ac/kku/cis/saveyouractivity/Mycode.kt
+++ app/src/main/java/th/ac/kku/cis/saveyouractivity/Mycode.kt
@@ -1,12 +1,22 @@
 package th.ac.kku.cis.saveyouractivity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_mycode.*
+import net.glxn.qrgen.android.QRCode
+
 
 class Mycode : AppCompatActivity() {
-
+    var USER:UserData= UserData()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mycode)
+        USER.UserData()
+        val myBitmap: Bitmap = QRCode.from(USER.getuid()).bitmap()
+        val myImage: ImageView = findViewById(R.id.code) as ImageView
+        myImage.setImageBitmap(myBitmap)
+        Name.setText(USER.getname())
     }
 }
